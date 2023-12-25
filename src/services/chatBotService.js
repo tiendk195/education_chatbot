644067d50f5c9cecab9ce932577bf8aaa9b2deb7
tiendk195 <tiendk195@gmail.com>
2023-12-25 20:26:22 +0700

    update v11

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -5,6 +5,31 @@ import request from "request";
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
+let getUserName = (sender_psid) => {
+  // Construct the message body
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        // qs: { access_token: PAGE_ACCESS_TOKEN },
+        method: "GET",
+      },
+      (err, res, body) => {
+        // console.log(body);
+        if (!err) {
+          body = JSON.parse(body);
+          let username = `${body.last_name} ${body.first_name}`;
+          resolve(username);
+        } else {
+          console.error("Unable to send message:" + err);
+          reject(err);
+        }
+      }
+    );
+  });
+  // Send the HTTP request to the Messenger Platform
+};
+
 let callSendAPI = (sender_psid, response) => {
   // Construct the message body
   let request_body = {
@@ -34,7 +59,10 @@ let callSendAPI = (sender_psid, response) => {
 let handleGetstarted = (sender_psid) => {
   return new Promise(async (resolve, reject) => {
     try {
-      response = { text: "Chào mừng bạn đến với Chat Bot UNETI." };
+      let username = await getUserName(sender_psid);
+      let response = {
+        text: `Chào mừng ${username} đến với Chat Bot UNETI!Bạn đang cần tìm kiếm thông tin gì?`,
+      };
       await callSendAPI(sender_psid, response);
       resolve("done");
     } catch (error) {
